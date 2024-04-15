855b139a1b9c8669925410e6f7360fa7b7321601
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-04-15 15:34:59 +0530

    SPLASH sCREEN

--- src/views/Splashscreen/Create/SplashScreenCreateHook.js
+++ src/views/Splashscreen/Create/SplashScreenCreateHook.js
@@ -148,7 +148,7 @@ console.log(selectVideos, "Videos")
         }
       }
       fd.append("event_id", "65029c5bdf6918136df27e51")
-      fd.append("id", id)
+    
       if (form?.status) {
         fd.append("status", form?.status ? "ACTIVE" : "INACTIVE");
       }
@@ -162,6 +162,7 @@ console.log(selectVideos, "Videos")
 
       let req;
       if (id) {
+        fd.append("id", id)
         req = serviceUpdateSplashScreen;
       } else {
         req = serviceCreateSplashScreen;
