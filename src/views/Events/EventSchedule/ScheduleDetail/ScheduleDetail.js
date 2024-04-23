18f885b6f5291af308cf1dd3d8cc145442cbe266
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-04-23 12:49:43 +0530

    Schedule Detail

--- src/views/Events/EventSchedule/ScheduleDetail/ScheduleDetail.js
+++ src/views/Events/EventSchedule/ScheduleDetail/ScheduleDetail.js
@@ -2,7 +2,17 @@ import React from "react";
 import PageBoxComponent from "../../../../components/PageBox/PageBox.component";
 
 import styles from "./Style.module.css";
+import SessionTable from "../../../../components/SectionTable/SectionTable";
 const ScheduleDetail = ({ handleScheduleDetail, isScheduleDetail, empId }) => {
+  const userData = [
+    {
+      name: "Abhay Gupta",
+      avatar: "..",
+      rating: 3,
+      comment: "comment added by user will be shown here",
+    },
+    
+  ];
   return (
     <div className={styles.pageContainer}>
       <PageBoxComponent>
@@ -13,42 +23,8 @@ const ScheduleDetail = ({ handleScheduleDetail, isScheduleDetail, empId }) => {
         </p>
         <p className={styles.timeing}>8 MAY | 11:00 AM - 12:00 PM</p>
       </PageBoxComponent>
-      <div style={{ marginTop: "20px" }}>
-        <PageBoxComponent>
-          <div className={styles.InfoWrap}>
-            <div>
-              <div>Session Rating</div>
-              <div className={styles.newLine}></div>
-            </div>
-            <div className={styles.timeing}>
-              Overall Rating
-              <br />4 ⭐ (120)
-            </div>
-          </div>
-          <div className={styles.tableContainer}>
-            <table className={styles.table}>
-              <tr className={styles.tableContainerBox}>
-                <th className={styles.th}>USER</th>
-                <th className={styles.th}>RATING</th>
-                <th className={styles.th}>COMMENT</th>
-              </tr>
-              <tbody>
-                <tr>
-                  <td className={styles.td}>
-                    <div className={styles.td1}>
-                      <img src="" />
-                      <div>Name</div>
-                    </div>
-                  </td>
-                  <td className={styles.td}>3</td>
-                  <td className={styles.td}>
-                    comment added by user will be shown here
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </PageBoxComponent>
+      <div className={"mt"} >
+        <SessionTable data={userData} />
       </div>
     </div>
   );
