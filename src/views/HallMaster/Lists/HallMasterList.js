f4cfd91bbe1ca661d56735fc83b0d2086f2d3de2
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-04-26 16:04:37 +0530

    [ TESTING ] Hall Master

--- src/views/HallMaster/Lists/HallMasterList.js
+++ src/views/HallMaster/Lists/HallMasterList.js
@@ -65,7 +65,7 @@ const HallMasterList = ({}) => {
         sortable: false,
         render: (temp, all) => (
           <div>
-            {all?.hall_no}
+            {all?.hall_no}.
           </div>
         ),
       },
